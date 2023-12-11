6a5fa64e55c7992d8a39816876dac99aebf98b51
Ryan D. Friese <ryan.friese@pnnl.gov>
2023-12-11 14:50:14 -0800

    add rt_exhange_data to rt_internal.h

--- src/include/rofi_internal.h
+++ src/include/rofi_internal.h
@@ -111,6 +111,7 @@ int rt_get_size(void);
 int rt_put(char*, void*, size_t);
 int rt_get(int, char*, void*, size_t);
 int rt_exchange(void);
+int rt_exchange_data(char *data_name, void *data, size_t size, void *recvbuf, unsigned int rank, unsigned int nranks);
 void rt_barrier(void);
 
 int            mr_init(void);
